11cbebf1f05cc60561ade9e0a4b12a91415d0a3d
michellekittleson <michelle.kittleson@gmail.com>
2024-07-30 11:42:22 -0500

    first commit

---
+++ 1_python_regex_deep_dive.py
@@ -0,0 +1,16 @@
+'''
+1. Python Regular Expressions Deep Dive
+
+Task 1: Email Extraction Enhancement
+
+Problem Statement: You have a script that extracts email addresses from a text but needs to be refined to exclude certain domains (e.g., '[exclude.com](http://exclude.com/)'). Modify the script to extract all email addresses except those from the specified domain.
+
+Adapt the regex pattern to exclude email addresses from '[exclude.com](http://exclude.com/)'.
+Ensure the script still extracts all other valid email addresses.
+
+'''
+
+import re
+
+text = "Emails: user1@domain.com, user2@exclude.com, user3@domain.com"
+emails = re.findall(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-(?!.*\bexclude\b)]+\.[A-Z|a-z]{2,}\b", text)
